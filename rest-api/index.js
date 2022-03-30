842c4f97b177073c676e810ed549e681ff388ffe
khukhapede <khusnan.hadieka@gmail.com>
2022-03-30 22:17:43 +0700

    little fix

--- rest-api/index.js
+++ rest-api/index.js
@@ -11,6 +11,6 @@ const port = 6000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/", router);
+app.use("/api/", router);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
